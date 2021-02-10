0f217a7b9fe3a207282ce1104b58d92bed6f2d40
Nathan Peek <nmp14fsu@gmail.com>
2021-02-09 23:18:58 -0500

    initial commit

--- index.js
+++ index.js
@@ -1,26 +1,16 @@
-const inquirer = require('inquirer');
+// array of questions for user
+const questions = [
 
+];
 
-inquirer
-    .prompt([
-        {
-            type: 'input',
-            name: 'name',
-            message: 'What is your name?',
-        },
-        {
-            type: 'checkbox',
-            message: 'What languages do you know?',
-            name: 'stack',
-            choices: ['HTML', 'CSS', 'JavaScript', 'MySQL'],
-        },
-        {
-            type: 'list',
-            message: 'What is your preferred method of communication?',
-            name: 'contact',
-            choices: ['email', 'phone', 'telekinesis'],
-        },
-    ])
-    .then((data) => {
-        console.log(data);
-    });
+// function to write README file
+function writeToFile(fileName, data) {
+}
+
+// function to initialize program
+function init() {
+
+}
+
+// function call to initialize program
+init();
