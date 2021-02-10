81570b6fab5708b7740d4211997674249bdbfc9c
Nathan Peek <nmp14fsu@gmail.com>
2021-02-09 23:26:25 -0500

    Add questions to questions array. Create basic inquirer prompt structure

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
+const inquirer = require("inquirer");
+const fs = require("fs");
+
 // array of questions for user
 const questions = [
-
+    "Title of Project", "Project Description", "Table of Contents", "Installation", "Usage", "Liscense", "Contributing", "Tests", "Questions"
 ];
 
 // function to write README file
@@ -9,7 +12,13 @@ function writeToFile(fileName, data) {
 
 // function to initialize program
 function init() {
-
+    inquirer
+        .prompt([
+            /* Pass your questions in here */
+        ])
+        .then(answers => {
+            // Use user feedback for... whatever!!
+        })
 }
 
 // function call to initialize program
