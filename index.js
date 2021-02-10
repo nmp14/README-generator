c98c8b1e1cb83a6ebb8d2761ef8818ef574b2b0d
Nathan Peek <nmp14fsu@gmail.com>
2021-02-09 23:13:25 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,26 @@
+const inquirer = require('inquirer');
+
+
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is your name?',
+        },
+        {
+            type: 'checkbox',
+            message: 'What languages do you know?',
+            name: 'stack',
+            choices: ['HTML', 'CSS', 'JavaScript', 'MySQL'],
+        },
+        {
+            type: 'list',
+            message: 'What is your preferred method of communication?',
+            name: 'contact',
+            choices: ['email', 'phone', 'telekinesis'],
+        },
+    ])
+    .then((data) => {
+        console.log(data);
+    });
